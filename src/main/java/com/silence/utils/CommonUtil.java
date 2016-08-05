d67b768dde141b767f3515171e55b991211d9273
silence940109 <695412269@qq.com>
2016-08-05 23:28:29 +0800

    common utils

---
+++ src/main/java/com/silence/utils/CommonUtil.java
@@ -0,0 +1,15 @@
+package com.silence.utils;
+
+public class CommonUtil {
+
+	public static int objectToInteger(Object obj) {
+		try {
+			if (obj != null && !obj.toString().trim().equals(""))
+				return Integer.parseInt(obj.toString());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return 0;
+		}
+		return 0;
+	}
+}
